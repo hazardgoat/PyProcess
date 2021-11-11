1cabba858fcdea5c7bce5c22cfb6a849dcf4ccb4
hazardgoat <74040471+hazardgoat@users.noreply.github.com>
2021-11-11 12:10:51 -0800

    Update example.py

--- example/example.py
+++ example/example.py
@@ -6,7 +6,7 @@ main_dir = /home/USER/Desktop
 project_dir = os.path.join(main_dir, 'MyProject')
 pp.setup.Project_Folders(project_dir)
 
-# Clean
+# Cleaning
 files_dir = os.path.join(project_dir, 'Data')
 replacements = {' +':',', '\t':','}
 pp.clean.Correct_Delimiter(files_dir, replacements, dir=True, concat_nl=True)
